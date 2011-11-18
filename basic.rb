ccb271f2d218a45f59adcca29cfaba625393fc8d
Priit Tamboom <priit@gitlab.eu>
2011-11-18 02:46:22 +0000

    added zip

--- basic.rb
+++ basic.rb
@@ -3,6 +3,8 @@ dep 'basic', :username do
 
   requires 'locales.conf'
   requires 'tree.managed'
+  requires 'zip.managed'
+  requires 'unzip.managed'
 
   requires 'zsh.conf'.with(username) if username != 'root'
   requires 'zsh.confroot'
@@ -10,3 +12,7 @@ dep 'basic', :username do
   requires 'vim.conf'.with(username) if username != 'root'
   requires 'vim.confroot'
 end
+
+deb 'tree.managed' 
+deb 'unzip.managed'
+deb 'zip.managed'
--- tree.rb
+++
@@ -1,1 +0,0 @@
-dep 'tree.managed'
