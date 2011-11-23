4b5cd44d390786dd297b88fb0ce121774e1357fb
Priit Tamboom <priit@gitlab.eu>
2011-11-23 11:55:03 +0200

    add group

--- user.rb
+++ user.rb
@@ -1,11 +1,17 @@
+dep 'group', :name do
+  met? { grep(/^#{name}:/, '/etc/group') }
+  meet { sudo "groupadd #{name}" }
+end
+
 dep 'user', :username, :password do
   requires 'zsh'
+  requires 'group'.with(username)
   password.ask("Additional user password")
 
   met? { grep(/^#{username}:/, '/etc/passwd') }
   meet {
     sudo "mkdir -p /home/#{username}" and
-    sudo "useradd -m -s /bin/zsh -b /home -G admin #{username}" and
+    sudo "useradd -m -s /bin/zsh -b /home -G #{username} #{username}" and
     sudo "chmod 701 /home/#{username}" and
     sudo %{echo "#{password}\n#{password}" | passwd #{username}}
   }
