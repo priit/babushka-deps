ebe28dc5b99fa6ee8d4255e1e458528208856dbb
Priit Tamboom <priit@gitlab.eu>
2011-11-23 16:14:48 +0200

    test

--- build-essential.rb
+++ build-essential.rb
@@ -2,6 +2,6 @@ dep 'build-essential.managed'
 dep 'binutils-doc.manage'
 
 dep 'build-essential' do
-  requires 'build-essential.managed'
-  requires 'binutils-doc.manage'
+  #requires 'build-essential.managed'
+  #requires 'binutils-doc.manage'
 end
