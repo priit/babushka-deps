6a3e18f1c990be0675a3a57e8a5d7ed9e84e88f6
Priit Tamboom <priit@gitlab.eu>
2011-11-23 13:19:27 +0200

    added env

---
+++ env.rb
@@ -0,0 +1,13 @@
+dep 'env' do
+  env.default('production').choose(%w[development staging production])
+
+  met? do
+    '/etc/development'.p.exists? ||
+    '/etc/staging'.p.exists? ||
+    '/etc/production'.p.exists?
+  end
+
+  meet do 
+    sudo "touch /etc/#{env}" 
+  end
+end
