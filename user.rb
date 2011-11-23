29a87f04124a1fc9c6b094136e983b8cd2393e56
Priit Tamboom <priit@gitlab.eu>
2011-11-23 11:08:16 +0200

    ask password

--- user.rb
+++ user.rb
@@ -1,11 +1,12 @@
 dep 'user', :username do
   requires 'zsh'
+  password.ask("Additional user password: ")
 
   met? { grep(/^#{username}:/, '/etc/passwd') }
   meet {
     sudo "mkdir -p /home/#{username}" and
     sudo "useradd -m -s /bin/zsh -b /home -G admin #{username}" and
     sudo "chmod 701 /home/#{username}" and
-    sudo "passwd #{username}"
+    sudo %{echo "#{password}\n#{password}" | passwd #{username}}
   }
 end
