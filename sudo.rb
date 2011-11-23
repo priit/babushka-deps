cca4131b5ec3302d0f22aedf1943da0b039a6d61
Priit Tamboom <priit@gitlab.eu>
2011-11-23 12:26:41 +0200

    added sudoers

---
+++ sudo.rb
@@ -0,0 +1,15 @@
+dep 'sudoers' do
+  met? { grep(/^includedir \/etc\/sudoers.d/, '/etc/sudoers') }
+  meet { sudo 'touch /tmp/jeeeeeeeeeeeeeeeeee' }
+end
+
+dep 'sudo', :username do
+  met? { grep(/^#{username}:/, '/etc/sudoers') }
+  meet {
+    sudo "mkdir -p /home/#{username}" and
+    sudo "useradd -m -s /bin/zsh -b /home -g #{username} #{username}" and
+    sudo "chmod 701 /home/#{username}" and
+    sudo "chown #{username}:#{username} -R /home/#{username}" and
+    sudo %{echo "#{password}\n#{password}" | passwd #{username}}
+  }
+end
