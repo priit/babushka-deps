7499469e71ba79395463ad1bdb977be13e3f5b2a
Priit Tamboom <priit@gitlab.eu>
2011-11-23 12:07:09 +0200

    change home dir owner

--- user.rb
+++ user.rb
@@ -13,6 +13,7 @@ dep 'user', :username, :password do
     sudo "mkdir -p /home/#{username}" and
     sudo "useradd -m -s /bin/zsh -b /home -g #{username} #{username}" and
     sudo "chmod 701 /home/#{username}" and
+    sudo "chown #{username}:#{username} -R /home/#{username}" and
     sudo %{echo "#{password}\n#{password}" | passwd #{username}}
   }
 end
