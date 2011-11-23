fa4cd559dacded9211a1febc49b5d910cdee2c6e
Priit Tamboom <priit@gitlab.eu>
2011-11-23 13:12:49 +0200

    simple sudo

--- sudo.rb
+++ sudo.rb
@@ -1,16 +1,6 @@
-dep 'sudoers.d' do
-  met? { grep(/^includedir \/etc\/sudoers.d/, '/etc/sudoers') }
-  meet { append_to_file 'includedir /etc/sudoers.d', '/etc/sudoers' }
-end
-
 dep 'sudo', :username do
-  requires 'sudoers.d'
-
-  met? { "/etc/sudoers.d/#{username}".p.exists? }
+  met? { grep(/^#{username}/, '/etc/sudoers') }
   meet do 
-    sudo "rm /etc/sudoers.d/#{username}"
-    sudo "touch /etc/sudoers.d/#{username}"
-    append_to_file "#{username} ALL=(ALL:ALL) ALL", "/etc/sudoers.d/#{username}" 
-    sudo "chmod 0440 /etc/sudoers.d/#{username}"
+    append_to_file "#{username} ALL=(ALL:ALL) ALL", "/etc/sudoers" 
   end
 end
