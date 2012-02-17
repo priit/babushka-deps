fee77e1a0603f5813f519dbbb4c8bc1af0ddaf44
Priit Tamboom <priit@gitlab.eu>
2012-02-17 12:18:42 +0200

    ups fixed wrong logic

--- ssh.rb
+++ ssh.rb
@@ -59,7 +59,7 @@ dep 'ssh.authorized_keys', :username, :key do
     if grep(/aa@aa|veiko@veiko/, "#{ssh_dir}/authorized_keys")
       true
     else
-      confirm('Do you like to add ssh keys?') == 'y'
+      confirm('Should we skip adding ssh keys?') == 'y'
     end
   end
 
@@ -90,7 +90,7 @@ dep 'ssh.authorized_keys-twice', :username, :key do
     if grep(/aa@aa|veiko@veiko/, "#{ssh_dir}/authorized_keys")
       true
     else
-      confirm('Do you like to add ssh keys?') == 'y'
+      confirm('Should we skip adding ssh keys?') == 'y'
     end
   end
 
