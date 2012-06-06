eb4eb2fe95846fa07e1b751bc3d042bb61be01fa
Priit Tamboom <priit@gitlab.eu>
2012-06-06 15:25:08 +0300

    turn screen visual bell off

--- screen.rb
+++ screen.rb
@@ -6,7 +6,8 @@ dep 'screen.conf', :username  do
   before { shell "touch /home/#{username}/.screenrc" }
   met? { username == 'root' ? true : grep(/^vbell on/, "/home/#{username}/.screenrc") }
   meet do
-    append_to_file 'vbell on', "/home/#{username}/.screenrc"
+    append_to_file 'vbell off', "/home/#{username}/.screenrc"
+    append_to_file 'startup_message off', "/home/#{username}/.screenrc"
     log_shell  "Set owner as #{username}:#{username}:", 
       "chown #{username}:#{username} /home/#{username}/.screenrc"
   end
@@ -16,7 +17,7 @@ dep 'screen.confroot' do
   before { shell "touch /root/.screenrc" }
   met? { grep(/^vbell on/, "/root/.screenrc") }
   meet do
-    append_to_file 'vbell on', "/root/.screenrc"
+    append_to_file 'vbell off', "/root/.screenrc"
     append_to_file 'startup_message off', '/etc/screenrc'
   end
 end
