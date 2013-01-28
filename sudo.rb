1faec719b0139d1bf8c6525727e456b8ea6175d7
Priit Tamboom <priit@gitlab.eu>
2013-01-28 14:08:47 +0200

    fix sudoers typo

--- sudo.rb
+++ sudo.rb
@@ -1,6 +1,6 @@
 dep 'sudo', :username do
   met? { '/etc/sudoers'.p.grep(/^#{username}/) }
   meet do 
-    '/etc/sudoers'.p.append("#{username} ALL=(ALL:ALL) ALL, NOPASSWD:/etc/init.d/apache2, /etc/init.d/nginx") 
+    '/etc/sudoers'.p.append("#{username} ALL=(ALL:ALL) ALL, NOPASSWD:/etc/init.d/apache2,/etc/init.d/nginx") 
   end
 end
