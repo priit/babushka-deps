43841a0468e17e66453d0e6a285a5cef831ff13a
Priit Tark <priit@gitlab.eu>
2014-12-08 13:35:15 +0200

    test

--- user.rb
+++ user.rb
@@ -3,21 +3,21 @@ dep 'app_user', :username, :password do
   username.ask("New app username'")
   password.ask("New password")
 
-  requires 'user'.with(username, password)
-  # requires 'user is sudoer'.with(name)
   met? { 
   }
   meet { 
+    requires 'user'.with(username, password)
+    # requires 'user is sudoer'.with(name)
   }
 end
 
 # basic user with zsh
 dep 'user', :username, :password do
-  requires 'zsh'
-  requires 'group'.with(username)
-
   met? { '/etc/passwd'.p.grep(/^#{username}:/) }
   meet {
+    requires 'zsh'
+    requires 'group'.with(username)
+
     sudo "useradd --create-home --shell /bin/zsh --base-dir /home -g #{username} #{username}" and
     sudo "chmod 701 /home/#{username}" and
     sudo "chown #{username}:#{username} -R /home/#{username}" and
