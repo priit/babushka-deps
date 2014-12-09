491e0be975f2d54c861065edd482d013f31aa69b
Priit Tark <priit@gitlab.eu>
2014-12-09 13:01:14 +0200

    test

--- custom.rb
+++ custom.rb
@@ -2,23 +2,27 @@ dep 'custom' do
   # set locale, US by default
   requires 'locales.conf'
 
-  # set server env
+  # set server env such as production
   requires 'env'
 
+  # add root zshrc
   requires 'root_user_zshrc'
 
-  # set priit vim
+  # add v bin and priit vim
   requires 'vimrc_priit'
 
   # cleanup ssh welcome text
   requires 'motd_empty'
+
+  # verify ssh UsePAM is off
+  requires 'sshd_pam_should_be_off'
 end
 
 
 # cleanup ssh welcome text
 dep 'motd_empty' do
   met? do
-    !"/etc/motd".p.exists?
+    "/etc/motd".p.empty?
   end
 
   meet do
