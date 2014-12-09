01c51b5cdfa52d6c4baabeb20e7573e12c10de3a
Priit Tark <priit@gitlab.eu>
2014-12-09 17:40:51 +0200

    test

--- custom.rb
+++ custom.rb
@@ -64,16 +64,23 @@ dep 'admin_password', :password do
   setup do
     unmeetable! 'This dep must be run as root.' unless shell('whoami') == 'root'
     if !confirm('Root user does not have password, should we add it? (y/n)', default: 'n')
-      unmeetable! 'Skipping root password' 
+      @skip = false 
     end
   end
 
   met? do
-    shell('sudo -n true')
+    return true if skip
+    shell('sudo -k') # expire an existing cached password
+    shell?('sudo -n true')
   end
 
   meet do
     password.ask('Root user password')
   end
+
+  def skip
+    @skip ||= true
+  end
+
 end
 
