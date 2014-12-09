51e0a8ce8ce6178cdf4c5f942d4756a44f3202fa
Priit Tark <priit@gitlab.eu>
2014-12-09 12:40:06 +0200

    test

--- custom.rb
+++ custom.rb
@@ -9,4 +9,19 @@ dep 'custom' do
 
   # set priit vim
   requires 'vimrc_priit'
+
+  # cleanup ssh welcome text
+  requires 'motd-empty'
+end
+
+
+# cleanup ssh welcome text
+dep 'motd_empty' do
+  met? do
+    "/etc/motd".p.present?
+  end
+
+  meet do
+    shell "mv /etc/motd /etc/motd.old"
+  end
 end
