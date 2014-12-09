efbd39ad76464553cb28282051a89cb08a0bcea4
Priit Tark <priit@gitlab.eu>
2014-12-09 17:50:36 +0200

    test

--- custom.rb
+++ custom.rb
@@ -64,6 +64,7 @@ dep 'admin_password', :password do
   setup do
     unmeetable! 'This dep must be run as root.' unless shell('whoami') == 'root'
     if confirm('Root user does not have password, should we add it? (y/n)', default: 'n')
+      puts 'anybody'
       @skip = false 
     end
     puts 'setup:'
