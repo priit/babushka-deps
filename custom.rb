d4c2e3cc3d72b97f79a040878279cc8e235b1cc6
Priit Tark <priit@gitlab.eu>
2014-12-10 10:16:13 +0200

    test

--- custom.rb
+++ custom.rb
@@ -72,6 +72,9 @@ dep 'admin_password', :password do
   met? do
     if @add_it
       shell('sudo -k') # expire an existing cached password
+      puts 'test'
+      puts shell('passwd --status admin')[1] == 'P'
+
       shell('passwd --status admin')[1] == 'P'
     else
       true
