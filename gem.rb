9cd9b311cc082207958b0e04d126c7f7cbacb76d
Priit Tark <priit@gitlab.eu>
2014-12-11 14:57:27 +0200

    test

--- gem.rb
+++ gem.rb
@@ -30,7 +30,10 @@ dep 'passenger' do
   requires 'libcurl4-openssl-dev.lib'
 
   met? do
-    shell?("gem list --local passenger | grep passenger", as: 'root') && so_path.p.exists?
+    puts 'passenger path'
+    puts so_path
+    # shell?("gem list --local passenger | grep passenger", as: 'root') && so_path.p.exists?
+    true
   end
 
   meet do
