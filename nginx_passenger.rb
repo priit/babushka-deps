6e5b69c0820cc71e97ab4d52d4860c66bf798460
Priit Tark <priit@gitlab.eu>
2014-12-29 17:33:41 +0200

    added passenger root

--- nginx_passenger.rb
+++ nginx_passenger.rb
@@ -5,4 +5,27 @@
 dep 'nginx_passenger' do
   requires 'nginx-extras.lib'
   requires 'passenger.lib'
+  requires 'enable-passenger-root'
+end
+
+dep 'enable-passenger-root' do
+  met? do
+    path.p.grep(/#{from_string}/)
+  end
+
+  meet do 
+    shell "cp #{path} #{path}.backup"
+    shell "sed 's/#{from_string}/#{to_string}/g' #{path}.backup > #{path}"
+  end
+  
+  def path
+    '/etc/nginx/nginx.conf'
+  end
+
+  def from_string
+    '# passenger_root'
+  end
+  def to_string
+    'passenger_root'
+  end
 end
