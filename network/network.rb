ca20842effc0f8e116902f2146f2e55c36840426
Priit Tark <priit@gitlab.eu>
2015-11-05 17:59:50 +0200

    update

--- network/network.rb
+++ network/network.rb
@@ -11,13 +11,13 @@ dep 'network_ip_failover', :ip_failover do
     vir_dev = "eth0:#{next_virtual_interface_nr}"
     
     conf =  
-      "\n# IP failover:\n" /
-      "auto eth0:0\n" /
-      "iface #{vir_dev} inet static\n" /
-      "  address #{ip_failover}\n" /
-      "  netmask 255.255.255.255\n\n" /
-      "  post-up /sbin/ifconfig #{vir_dev} #{ip_failover} netmask 255.255.255.255 broadcast #{ip_failover}\n" /
-      "  pre-down /sbin/ifconfig #{vir_dev} down"
+      "\n# IP failover:\n" \
+      "auto #{vir_dev}\n" \
+      "iface #{vir_dev} inet static\n" \
+      "  address #{ip_failover}\n" \
+      "  netmask 255.255.255.255\n\n" \
+      "  post-up /sbin/ifconfig #{vir_dev} #{ip_failover} netmask 255.255.255.255 broadcast #{ip_failover}\n" \
+      "  pre-down /sbin/ifconfig #{vir_dev} down\n"
     path.p.append(conf)
   end
   # after { shell '/etc/init.d/networking restart' }
