d4c1047069118fed503ce66740f276159a59d4eb
Priit Tark <priit@gitlab.eu>
2015-11-05 17:56:09 +0200

    added ip failover conf

---
+++ network/network.rb
@@ -0,0 +1,28 @@
+dep 'network_ip_failover', :ip_failover do
+  met? do
+    path.p.grep(/^# IP failover:/)
+  end
+
+  meet do 
+    shell "cp #{path} #{path}.backup"
+    ip_failover.ask('Please provide failover ip:')
+    net_dev = 'eth0'
+    next_virtual_interface_nr = shell "grep -e 'iface[ \t]*#{net_dev}:' #{path} | wc --lines"
+    vir_dev = "eth0:#{next_virtual_interface_nr}"
+    
+    conf =  
+      "\n# IP failover:\n" /
+      "auto eth0:0\n" /
+      "iface #{vir_dev} inet static\n" /
+      "  address #{ip_failover}\n" /
+      "  netmask 255.255.255.255\n\n" /
+      "  post-up /sbin/ifconfig #{vir_dev} #{ip_failover} netmask 255.255.255.255 broadcast #{ip_failover}\n" /
+      "  pre-down /sbin/ifconfig #{vir_dev} down"
+    path.p.append(conf)
+  end
+  # after { shell '/etc/init.d/networking restart' }
+  
+  def path
+    '/etc/network/interfaces'
+  end
+end
