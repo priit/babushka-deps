cb2adfaa75b2a5f788545ea91e2a8ff72eca8758
Priit Tark <priit@gitlab.eu>
2015-11-06 12:07:54 +0200

    added password

--- server_db.rb
+++ server_db.rb
@@ -1,13 +1,14 @@
 # 
 # DB basic template
 #
-dep 'server_db' do
+dep 'server_db', :password do
   require 'ostruct'
   require 'yaml'
   conf = OpenStruct.new(YAML.load_file('babushka.yml')['server_db'])
+  password.ask("New password")
 
   requires 'general'
-  requires 'user'.with(conf.user)
+  requires 'user'.with(conf.user, password)
   requires 'debian_custom'
   requires 'network_ip_failover'
 end
