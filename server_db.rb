3a8ff087baf00644f58b337300315702a25477ef
Priit Tark <priit@gitlab.eu>
2015-11-06 12:05:47 +0200

    Added basic conf support

--- server_db.rb
+++ server_db.rb
@@ -1,12 +1,13 @@
 # 
 # DB basic template
 #
-dep 'server_db', :username, :password do
-  username.ask("New app username'")
-  password.ask("New password")
+dep 'server_db' do
+  require 'ostruct'
+  require 'yaml'
+  conf = OpenStruct.new(YAML.load_file('babushka.yml')['server_db'])
 
   requires 'general'
-  requires 'user'.with(username, password)
+  requires 'user'.with(conf.username)
   requires 'debian_custom'
   requires 'network_ip_failover'
 end
