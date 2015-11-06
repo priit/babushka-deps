e28f5018ffbf57e63ea3e52af062379bbcede9f8
Priit Tark <priit@gitlab.eu>
2015-11-06 13:39:45 +0200

    improve message

--- server_db.rb
+++ server_db.rb
@@ -16,7 +16,7 @@ dep 'server_db', :password do
     requires 'network_ip_failover'
   else
     requires 'server_db_yml'
-    puts 'Please edit babushka.yml file before continue'
+    puts '/n/nPlease edit babushka.yml file before continue/n'
   end
 end
 
