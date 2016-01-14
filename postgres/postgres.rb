555fa2258e411e200d38284661d96e5c6f75e866
Priit Tark <priit@gitlab.eu>
2016-01-14 10:33:05 +0200

    test

--- postgres/postgres.rb
+++ postgres/postgres.rb
@@ -1,9 +1,12 @@
-dep 'postgresql.bin'
-dep 'libpq-dev.bin'
+dep 'postgresql.bin' do 
+  installs 'postgres-9.4'
+end
+
+dep 'libpq-dev.managed'
 
 dep 'postgres' do
   requires 'postgresql.bin'
-  requires 'libpq-dev.bin'
+  requires 'libpq-dev.managed'
 end
 
 dep 'postgres94', template: 'bin' do
