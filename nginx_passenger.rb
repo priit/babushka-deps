4138d78922d43fb5e0cd3df76462a6b11d858b69
Priit Tark <priit@gitlab.eu>
2017-03-15 14:41:32 +0200

    enable passenger

--- nginx_passenger.rb
+++ nginx_passenger.rb
@@ -5,11 +5,10 @@
 dep 'nginx_passenger' do
   requires 'nginx.lib'
   requires 'passenger.lib'
-  requires 'enable-passenger-root'
-  requires 'enable-passenger-ruby'
+  requires 'enable-passenger'
 end
 
-dep 'enable-passenger-root' do
+dep 'enable-passenger' do
   met? do
     !path.p.grep(/#{from_string}/)
   end
@@ -24,33 +23,10 @@ dep 'enable-passenger-root' do
   end
 
   def from_string
-    '# passenger_root'
+    '# include /etc/nginx/passenger.conf;'
   end
 
   def to_string
-    'passenger_root'
-  end
-end
-
-dep 'enable-passenger-ruby' do
-  met? do
-    !path.p.grep(/#{from_string}/)
-  end
-
-  meet do 
-    shell "cp #{path} #{path}.backup"
-    shell "sed 's/#{from_string}/#{to_string}/g' #{path}.backup > #{path}"
-  end
-  
-  def path
-    '/etc/nginx/nginx.conf'
-  end
-
-  def from_string
-    '# passenger_ruby'
-  end
-
-  def to_string
-    'passenger_ruby'
+    'include /etc/nginx/passenger.conf;'
   end
 end
