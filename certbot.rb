05a2e6ce11c1589d012fba17c6cd4cfa397bd9ed
Priit Tark <priit@gitlab.eu>
2017-03-15 17:45:01 +0200

    create path

--- certbot.rb
+++ certbot.rb
@@ -4,4 +4,12 @@
 #
 dep 'certbot' do
   requires 'apt-backports-list'
+  requires 'certbot_create_webroot_path'
+end
+
+dep 'certbot_create_webroot_path' do
+  met? { "/usr/share/nginx/html/.well-known/acme-challenge".p.exists? }
+  meet do
+    shell "mkdir -p /usr/share/nginx/html/.well-known/acme-challenge"
+  end
 end
